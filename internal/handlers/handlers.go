db9b9d53b8c3ea6ca992114329d79b6aea51948e
JOSTIN ENRIQUE ALVARADO SARMIENTO <jostinalvaradosarmiento@gmail.com>
2025-10-21 17:50:50 -0500

    feat: enhance CORS middleware for improved security and flexibility in handling requests

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -17,7 +17,7 @@ func corsMiddleware(next http.Handler) http.Handler {
 		w.Header().Set("Vary", "Origin")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH")
 
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusNoContent)
